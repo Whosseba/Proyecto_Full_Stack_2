dcd2b6aae80018b24b61297db6ba9c1f9d4d91b5
Felipe Gonzalez <felipe.gonzalez@duocuc.cl>
2025-10-13 16:29:24 -0300

    Agregado de 1 teclado y 2 productos

--- src/contextos/ContextoProductos.jsx
+++ src/contextos/ContextoProductos.jsx
@@ -5,8 +5,13 @@ import tecladoImg from "../imagenes/accesorios/teclado.jpg";
 import monitorImg from "../imagenes/accesorios/monitor.jpg";
 import gabineteImg from "../imagenes/accesorios/gabinete.jpg";
 import mouse2Img from "../imagenes/accesorios/mouse2.jpg";
+import teclado2Img from "../imagenes/accesorios/teclado2.jpg";
+import laptop2Img from "../imagenes/laptops/laptop2.jpg";
+import laptop3Img from "../imagenes/laptops/laptop3.jpg";
 
 export const ContextoProductos = createContext();
+
+// Nombres de las categorias: mouses, laptops, teclados, monitores, gabinetes
 
 export const ProveedorProductos = ({ children }) => {
   const [productos, setProductos] = useState([
@@ -42,7 +47,7 @@ export const ProveedorProductos = ({ children }) => {
     { 
       id: 3,
       nombre: "Teclado Mecánico",
-      precio: 120,
+      precio: 12000,
       descripcion: "Teclado mecánico retroiluminado",
       categoria: "teclados",
       imagen: tecladoImg,
@@ -86,7 +91,7 @@ export const ProveedorProductos = ({ children }) => {
       nombre: "Mouse Hp M10",
       precio: 45,
       descripcion: "Mouse Alámbrico Hp M10 Negro Bk",
-      categoria: "mouse",
+      categoria: "mouses",
       imagen: mouse2Img,
       especificaciones: [
         "Resolución del sensor de 1200 dpi para un seguimiento preciso y fluido.",
@@ -95,7 +100,66 @@ export const ProveedorProductos = ({ children }) => {
         "Tres botones para un control eficiente.",
         "Velocidad máxima de 1200 ips para una respuesta rápida."
       ]
+    },
+    { 
+      id: 7,
+      nombre: "Teclado Gamer Kb892l ",
+      precio: 19390,
+      descripcion: "Teclado Gamer Mecánico Luces Rgb Kb892l Havit",
+      categoria: "teclados",
+      imagen: teclado2Img,
+      especificaciones: [
+        "Tipo de switch: Mecánico.",
+        "Idioma: Inglés US.",
+        "Layout: QWERTY.",
+        "Color de la retroiluminación: RGB.",
+        "Color del teclado: Negro.",
+        "Función antighosting incorporada.",
+        "Tipo de teclado: mecánico."
+      ]
+    },
+    { 
+      id: 8,
+      nombre: "Notebook Lenovo Ideapad Slim 3",
+      precio: 539990,
+      descripcion: "Notebook Lenovo Ideapad Slim 3 Gen 10 Core I5 13420h 8g+512g",
+      categoria: "laptops",
+      imagen: laptop2Img,
+      especificaciones: [
+        "Procesador: Intel Core i5 13420H.",
+        "Versión del sistema operativo: 11.",
+        "Edición del sistema operativo: Home.",
+        "Nombre del sistema operativo: Windows.",
+        "Capacidad del disco rígido: 0 MB.",
+        "Capacidad de disco SSD: 512 GB.",
+        "Con pantalla táctil: No.",
+        "Resolución de la pantalla: 1920 px x 1200 px.",
+        "Conexión wifi y bluetooth.",
+        "Con teclado retroiluminado."
+      ]
+    },
+    { 
+      id: 9,
+      nombre: "Notebook Nitro V Acer Gamer",
+      precio: 771351,
+      descripcion: "Notebook Nitro V Acer Gamer Intel Core I5 16 Gb 512 Ssd rtx 3050 Color Negro ANV15-51-53TQ-1",
+      categoria: "laptops",
+      imagen: laptop3Img,
+      especificaciones: [
+        "Procesador: Intel Core i5 13420H.",
+        "Versión del sistema operativo: 11.",
+        "Edición del sistema operativo: Home.",
+        "Nombre del sistema operativo: Windows.",
+        "Capacidad de disco SSD: 512 GB.",
+        "Capacidad total del módulo de memoria RAM: 16 GB.",
+        "Con pantalla táctil: No.",
+        "Resolución de la pantalla: 1920 px x 1080 px.",
+        "Con teclado retroiluminado.",
+        "Resolución de 1920x1080 px.",
+        "Tarjeta gráfica RTX 3050."
+      ]
     }
+          
   ]);
 
   const agregarProducto = (nuevo) => {
