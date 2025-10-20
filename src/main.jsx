af650d2146a1d2721333371110493d21cbf70fba
whosseba <s.barriga@duocuc.cl>
2025-10-20 02:44:39 -0300

    dependencias agregadas , pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";                 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import './index.css';  
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import "./index.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Proyecto_Full_Stack_2/">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto_Full_Stack_2/', 
+  base: '/Proyecto_Full_Stack_2/',
   test: {
-    environment: 'jsdom', // Simula el DOM en los tests
+    environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.js'
   }
-})
+});
